15cc26267ccb52c90738b014f25eafd88dc986e6
milomacphail <milomacphail@gmail.com>
2019-08-19 10:47:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace YahtzeeOne
+{
+    class Dice
+    {
+        Random diceNumber = new Random();
+
+        public void rollDice()
+        {
+            int diceRoll = diceNumber.Next(1, 6);
+            Console.WriteLine(diceRoll);
+        }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dice gameRoll = new Dice();
+            gameRoll.rollDice();
+        }
+    }
+}
